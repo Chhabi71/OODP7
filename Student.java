501b3c1b3712f2aa927c127efe231bc0e73aa9b7
Chhabi71 <chhabim0608@gmail.com>
2026-01-05 17:50:35 +0545

    Added getter methods to Student class.

---
+++ Student.java
@@ -0,0 +1,22 @@
+package workshops7;
+
+
+public class Student {
+private final String name;
+private final int age;
+
+public Student(String name, int age){
+this.name = name;
+this.age = age;
+}
+public String getName(){
+return name;
+}
+public int getAge(){
+return age;
+}
+public void displayStudent(){
+System.out.println("Name: "+name);
+System.out.println("Age: "+age);
+}
+}
